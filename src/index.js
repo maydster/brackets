4568f1aadce8e5138ddb89a9bbf3109a0d091921
”Arthur <arthur.maydanuk@gmail.com>
2021-02-27 23:34:38 +0200

    init: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+
+  for(let char of str) {
+    
+    for(let item of bracketsConfig) {
+
+      if(char == item[0] && item[0] != item[1]) { // test for opening brackets
+        stack.push(char);
+      } else if(char == item[1] && item[0] != item[1]) { // test for closing brackets
+        let temp = stack.pop();
+        if(temp != item[0]) return false;
+      } else if (char == item[0] && item[0] == item[1]){ // test for '|'
+        if(stack[stack.length-1] != item[0]) {
+          stack.push(char);
+        } else {
+          stack.pop();
+        }
+      }
+
+    }
+
+  }
+
+  return !stack.length;
+
 }
